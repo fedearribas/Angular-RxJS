08191d658ed04b3dd4de060435c75f4f532126e6
farribas <farribas@emmsa.net>
2023-02-03 16:11:20 -0300

    Added async pipe. Added declarative approach for observables

---
+++ src/app/products/product-list-alt/product-list-alt.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { catchError, EMPTY } from 'rxjs';
+
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'pm-product-list',
+  templateUrl: './product-list-alt.component.html'
+})
+export class ProductListAltComponent {
+  pageTitle = 'Products';
+  errorMessage = '';
+  selectedProductId = 0;
+  
+  products$ = this.productService.products$
+  .pipe(
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
+
+  constructor(private productService: ProductService) { }
+
+  onSelected(productId: number): void {
+    console.log('Not yet implemented');
+  }
+}
